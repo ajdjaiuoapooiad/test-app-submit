6866d5347de841cb7d0a1622fa298ede33fe48ac
yukito <iyggf66974@yahoo.ne.jp>
2025-04-10 12:39:48 +0900

    修正

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -20,11 +20,11 @@ export default function TestPage() {
     <div>
 
 
-        <h1>buttonPage</h1>
-        <button className='bg-gray-300 m-20 p-5' onClick={handleClick}>確認</button>
+        <h1 className='text-3xl'>buttonPage</h1>
+        <button className='bg-gray-300 m-20 p-5 rounded-md' onClick={handleClick}>確認</button>
 
 
-        
+
     </div>
   );
 }
